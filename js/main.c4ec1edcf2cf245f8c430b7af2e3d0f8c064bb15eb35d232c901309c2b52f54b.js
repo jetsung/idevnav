58228aa8d9f3a1533005820eabc510a9c3660b16
jetsung <jetsung@users.noreply.github.com>
2026-07-28 19:44:40 +0800

    deploy: 796ad6351ddc3306bc3172ad09cd0f8ccc6b6f82

---
+++ js/main.c4ec1edcf2cf245f8c430b7af2e3d0f8c064bb15eb35d232c901309c2b52f54b.js
@@ -0,0 +1,23 @@
+(()=>{var n=`
+  color: #1772F6;
+  font-weight: bold;
+  font-size: 3em;
+  padding: 10px 0;
+  text-shadow: 0.7px 1px 0 rgb(255 255 255 / 100%),
+    1.4px 2px 0 rgb(255 255 255 / 96%),
+    2.1px 3px 0 rgb(255 255 255 / 92%),
+    2.8px 4px 0 rgb(255 255 255 / 88%),
+    1px 1px 2px rgb(100 100 100 / 70%);
+`;window.console.log("%cNavSide",n);var o=`
+  font-size: 1.2em;
+`;window.console.log(`%cNavSide \u7F51\u5740\u5BFC\u822A\u{1F970}
+
+\u57FA\u4E8E Hugo \u9759\u6001\u53D1\u5E03\u5668\u7684\u7F51\u5740\u5BFC\u822A\u7CFB\u7EDF\u{1F97A}
+
+\u5FEB\u901F\u90E8\u7F72\u{1F447}
+
+https://github.com/idevsig/navside
+
+https://i.jetsung.com https://github.com/jetsung
+
+`,o);})();
